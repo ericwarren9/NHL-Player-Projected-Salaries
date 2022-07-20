a14cfb456c40f0792e8f180da02abc61ffbe4805
Eric Warren <erwarren@ncsu.edu>
2022-07-19 22:46:14 -0400

    Random Forest EDA

--- NHL Salary EDA Random Forest.R
+++ NHL Salary EDA Random Forest.R
@@ -37,11 +37,98 @@ randomNHLRegression
 library(vip)
 vip(randomNHLRegression,
     geom = "col",
-    num_features = 370L) +
+    num_features = 100L) +
   theme_bw()
 
 # Table of the most important variables
-randomNHLRegression$variable.importance
+vi_scores <- vi(randomNHLRegression,
+                sort = T,
+                decreasing = T,
+                rank = T) %>%
+  top_n(-100)
+
+
+# Do the variance importance by forward -----------------------------------
+salary21SubsetForward <- salary21Subset %>%
+  filter(position == 2) %>%
+  select(-position)
+
+set.seed(9)
+randomNHLRegressionForward <- ranger(cap_hit ~ .,
+                              salary21SubsetForward,
+                              importance = "impurity",
+                              num.trees = 400,
+                              mtry = ncol(salary21SubsetForward) / 3)
+randomNHLRegressionForward
+
+vip(randomNHLRegressionForward,
+    geom = "col",
+    num_features = 100L) +
+  theme_bw()
+
+vi_scoresForward <- vi(randomNHLRegressionForward,
+                sort = T,
+                decreasing = T,
+                rank = T) %>%
+  top_n(-100)
+
+
+# Do the variance importance by defense -----------------------------------
+salary21SubsetDefense <- salary21Subset %>%
+  filter(position == 1) %>%
+  select(-position)
+
+set.seed(9)
+randomNHLRegressionDefense <- ranger(cap_hit ~ .,
+                                     salary21SubsetDefense,
+                                     importance = "impurity",
+                                     num.trees = 400,
+                                     mtry = ncol(salary21SubsetDefense) / 3)
+randomNHLRegressionDefense
+
+vip(randomNHLRegressionDefense,
+    geom = "col",
+    num_features = 100L) +
+  theme_bw()
+
+vi_scoresDefense <- vi(randomNHLRegressionDefense,
+                       sort = T,
+                       decreasing = T,
+                       rank = T) %>%
+  top_n(-100)
+
+# See what variables show up the most for whole dataset and position data
+importantLassoNames <- readRDS("RawData/lasso_important_names.rds")
+importantLassoNames <- as.data.frame(importantLassoNames) %>%
+  rename(Variable = importantLassoNames)
+
+# All data
+knownNames <- rbind(as.tibble(importantLassoNames$Variable),
+                    as.tibble(vi_scores$Variable))
+mostImportantVariables <- knownNames %>%
+  count(value) %>%
+  arrange(desc(n))
+
+# Forward Data
+importantLassoNamesForward <- readRDS("RawData/lasso_important_names_forward.rds")
+importantLassoNamesForward <- as.data.frame(importantLassoNamesForward) %>%
+  rename(Variable = importantLassoNamesForward)
+knownNamesForward <- rbind(as.tibble(importantLassoNamesForward$Variable),
+                    as.tibble(vi_scoresForward$Variable))
+mostImportantVariablesForward <- knownNamesForward %>%
+  count(value) %>%
+  arrange(desc(n))
+
+# Defense Data
+importantLassoNamesDefense <- readRDS("RawData/lasso_important_names_defense.rds")
+importantLassoNamesDefense <- as.data.frame(importantLassoNamesDefense) %>%
+  rename(Variable = importantLassoNamesDefense)
+knownNamesDefense <- rbind(as.tibble(importantLassoNamesForward$Variable),
+                           as.tibble(vi_scoresDefense$Variable))
+mostImportantVariablesDefense <- knownNamesDefense %>%
+  count(value) %>%
+  arrange(desc(n))
+
 
 # Tune the random forests
 library(caret)
