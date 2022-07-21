c3d86234cc49590f5b0540db090a02780ff3bd94
Eric Warren <erwarren@ncsu.edu>
2022-07-20 22:36:11 -0400

    Random Forest EDA

--- NHL Salary EDA Random Forest.R
+++ NHL Salary EDA Random Forest.R
@@ -4,22 +4,20 @@
 # Get data set up ---------------------------------------------------------
 
 library(tidyverse)
-salary21 <- read_csv("UsedDataForProject/NHL Player Stats and Salary 2021-22.csv")
-
-salary21Subset <- salary21 %>%
-  select(-c(player,
-            cap_hit_percent,
-            season))
+salaryAllSeasons <- read_csv("UsedDataForProject/NHL Player Stats and Salary Per 60 Minutes and Standardized 2019-22.csv")
 
 library(stats)
 # Change to dummy data set
-salary21Subset <- salary21Subset %>%
+salaryAllSeasonsSubset <- salaryAllSeasons %>%
+  select(-c(player,
+            cap_hit_percent,
+            season)) %>%
   filter(games_played >= 20,
          type != "Entry_Level")
 # Change to factor data from character
-salary21Subset[sapply(salary21Subset, is.character)] <- lapply(salary21Subset[sapply(salary21Subset, is.character)], as.factor)
+salaryAllSeasonsSubset[sapply(salaryAllSeasonsSubset, is.character)] <- lapply(salaryAllSeasonsSubset[sapply(salaryAllSeasonsSubset, is.character)], as.factor)
 # Change to numeric data
-salary21Subset[sapply(salary21Subset, is.factor)] <- lapply(salary21Subset[sapply(salary21Subset, is.factor)], as.numeric)
+salaryAllSeasonsSubset[sapply(salaryAllSeasonsSubset, is.factor)] <- lapply(salaryAllSeasonsSubset[sapply(salaryAllSeasonsSubset, is.factor)], as.numeric)
 
 
 # Do random forest regression ---------------------------------------------
@@ -28,16 +26,16 @@ salary21Subset[sapply(salary21Subset, is.factor)] <- lapply(salary21Subset[sappl
 set.seed(9)
 library(ranger)
 randomNHLRegression <- ranger(cap_hit ~ .,
-                              salary21Subset,
+                              salaryAllSeasonsSubset,
                               importance = "impurity",
-                              mtry = ncol(salary21Subset) / 3)
+                              mtry = ncol(salaryAllSeasonsSubset) / 3)
 randomNHLRegression
 
 # Find the variable importance
 library(vip)
 vip(randomNHLRegression,
     geom = "col",
-    num_features = 100L) +
+    num_features = 76L) +
   theme_bw()
 
 # Table of the most important variables
@@ -45,57 +43,55 @@ vi_scores <- vi(randomNHLRegression,
                 sort = T,
                 decreasing = T,
                 rank = T) %>%
-  top_n(-100)
+  top_n(-50)
 
 
 # Do the variance importance by forward -----------------------------------
-salary21SubsetForward <- salary21Subset %>%
+salaryAllSeasonsSubsetForward <- salaryAllSeasonsSubset %>%
   filter(position == 2) %>%
   select(-position)
 
 set.seed(9)
 randomNHLRegressionForward <- ranger(cap_hit ~ .,
-                              salary21SubsetForward,
+                              salaryAllSeasonsSubsetForward,
                               importance = "impurity",
-                              num.trees = 400,
-                              mtry = ncol(salary21SubsetForward) / 3)
+                              mtry = ncol(salaryAllSeasonsSubsetForward) / 3)
 randomNHLRegressionForward
 
 vip(randomNHLRegressionForward,
     geom = "col",
-    num_features = 100L) +
+    num_features = 76L) +
   theme_bw()
 
 vi_scoresForward <- vi(randomNHLRegressionForward,
                 sort = T,
                 decreasing = T,
                 rank = T) %>%
-  top_n(-100)
+  top_n(-50)
 
 
 # Do the variance importance by defense -----------------------------------
-salary21SubsetDefense <- salary21Subset %>%
+salaryAllSeasonsSubsetDefense <- salaryAllSeasonsSubset %>%
   filter(position == 1) %>%
   select(-position)
 
 set.seed(9)
 randomNHLRegressionDefense <- ranger(cap_hit ~ .,
-                                     salary21SubsetDefense,
+                                     salaryAllSeasonsSubsetDefense,
                                      importance = "impurity",
-                                     num.trees = 400,
-                                     mtry = ncol(salary21SubsetDefense) / 3)
+                                     mtry = ncol(salaryAllSeasonsSubsetDefense) / 3)
 randomNHLRegressionDefense
 
 vip(randomNHLRegressionDefense,
     geom = "col",
-    num_features = 100L) +
+    num_features = 76L) +
   theme_bw()
 
 vi_scoresDefense <- vi(randomNHLRegressionDefense,
                        sort = T,
                        decreasing = T,
                        rank = T) %>%
-  top_n(-100)
+  top_n(-50)
 
 # See what variables show up the most for whole dataset and position data
 importantLassoNames <- readRDS("RawData/lasso_important_names.rds")
@@ -105,6 +101,7 @@ importantLassoNames <- as.data.frame(importantLassoNames) %>%
 # All data
 knownNames <- rbind(as.tibble(importantLassoNames$Variable),
                     as.tibble(vi_scores$Variable))
+
 mostImportantVariables <- knownNames %>%
   count(value) %>%
   arrange(desc(n))
@@ -133,15 +130,15 @@ mostImportantVariablesDefense <- knownNamesDefense %>%
 # Tune the random forests
 library(caret)
 rf_tune_grid <- 
-  expand.grid(mtry = seq(3, 150, by = 3), 
+  expand.grid(mtry = seq(3, 75, by = 3), 
               splitrule = "variance",
               min.node.size = 5)
 set.seed(9)
 caret_nhl_rf <- 
   train(cap_hit ~ ., 
-        data = salary21Subset,
+        data = salaryAllSeasonsSubset,
         method = "ranger", 
-        num.trees = 150,
+        num.trees = 75,
         trControl = trainControl(method = "cv", number = 5),
         tuneGrid = rf_tune_grid)
 ggplot(caret_nhl_rf) + theme_bw()
@@ -160,16 +157,16 @@ xgboost_tune_control <- trainControl(method = "cv", number = 5, verboseIter = FA
 
 set.seed(9)
 
-xgb_tune <- train(x = as.matrix(select(salary21Subset, -cap_hit)),
-                  y = salary21Subset$cap_hit,
+xgb_tune <- train(x = as.matrix(select(salaryAllSeasonsSubset, -cap_hit)),
+                  y = salaryAllSeasonsSubset$cap_hit,
                   trControl = xgboost_tune_control,
                   tuneGrid = xgboost_tune_grid, 
                   objective = "reg:squarederror",
                   method = "xgbTree",
                   verbose = TRUE)
 
-xgb_fit_final <- xgboost(data = as.matrix(select(salary21Subset, -cap_hit)),
-                         label = salary21Subset$cap_hit, objective = "reg:squarederror",
+xgb_fit_final <- xgboost(data = as.matrix(select(salaryAllSeasonsSubset, -cap_hit)),
+                         label = salaryAllSeasonsSubset$cap_hit, objective = "reg:squarederror",
                          nrounds = xgb_tune$bestTune$nrounds,
                          params = as.list(select(xgb_tune$bestTune,
                                                  -nrounds)), 
