2b4148043da93f9da9b096d3a8da85ca8389b427
Eric Warren <erwarren@ncsu.edu>
2022-07-20 22:35:57 -0400

    Lasso/Ridge Regression EDA

--- NHL Salary EDA Lasso:Ridge Regression.R
+++ NHL Salary EDA Lasso:Ridge Regression.R
@@ -4,36 +4,31 @@
 # Load in needed things ---------------------------------------------------
 
 library(tidyverse)
-salary21 <- read_csv("UsedDataForProject/NHL Player Stats and Salary 2021-22.csv")
+salaryAllSeasons <- read_csv("UsedDataForProject/NHL Player Stats and Salary Per 60 Minutes and Standardized 2019-22.csv")
 
-
-# Subset data to get rid of player's name and cap hit % -------------------
-
-salary21Subset <- salary21 %>%
-  select(-c(player,
-            cap_hit_percent,
-            season))
 
 # Dimension Reduction Filtering for games and contract type ---------------
 
 library(stats)
 # Change to dummy data set
-salary21Subset <- salary21Subset %>%
+salaryAllSeasonsSubset <- salaryAllSeasons %>%
+  select(-c(player,
+            season)) %>%
   filter(games_played >= 20,
          type != "Entry_Level")
 # Change to factor data from character
-salary21Subset[sapply(salary21Subset, is.character)] <- lapply(salary21Subset[sapply(salary21Subset, is.character)], as.factor)
+salaryAllSeasonsSubset[sapply(salaryAllSeasonsSubset, is.character)] <- lapply(salaryAllSeasonsSubset[sapply(salaryAllSeasonsSubset, is.character)], as.factor)
 # Change to numeric data
-salary21Subset[sapply(salary21Subset, is.factor)] <- lapply(salary21Subset[sapply(salary21Subset, is.factor)], as.numeric)
+salaryAllSeasonsSubset[sapply(salaryAllSeasonsSubset, is.factor)] <- lapply(salaryAllSeasonsSubset[sapply(salaryAllSeasonsSubset, is.factor)], as.numeric)
 
 
 # Check what type of dimension is the best --------------------------------
 
 # Make the needed matrices
-model_x <- salary21Subset %>%
+model_x <- salaryAllSeasonsSubset %>%
   select(-cap_hit) %>%
   as.matrix()
-model_y <- salary21Subset$cap_hit
+model_y <- salaryAllSeasonsSubset$cap_hit
 
 # Make the ridge fit plot
 library(glmnet)
@@ -54,13 +49,14 @@ which.min(c(min(cv_en_25$cvm), min(cv_en_50$cvm), min(cv_en_75$cvm), min(cv_ridg
 
 # Do comparison of models for holdout performance
 set.seed(9)
-salary21Subset <- salary21Subset %>% mutate(test_fold = sample(rep(1:5, length.out = n())))
+salaryAllSeasonsSubset <- salaryAllSeasonsSubset %>% mutate(test_fold = sample(rep(1:5, length.out = n())))
+library(purrr)
 holdout_predictions <- 
-  map_dfr(unique(salary21Subset$test_fold), 
+  map_dfr(unique(salaryAllSeasonsSubset$test_fold), 
           function(holdout) {
             # Separate test and training data:
-            test_data <- salary21Subset %>% filter(test_fold == holdout)
-            train_data <- salary21Subset %>% filter(test_fold != holdout)
+            test_data <- salaryAllSeasonsSubset %>% filter(test_fold == holdout)
+            train_data <- salaryAllSeasonsSubset %>% filter(test_fold != holdout)
             # Repeat for matrices
             test_x <- as.matrix(dplyr::select(test_data, -cap_hit))
             train_x <- as.matrix(dplyr::select(train_data, -cap_hit))
@@ -107,12 +103,12 @@ saveRDS(importantLassoNames, file = "RawData/lasso_important_names.rds")
 # Forward Lasso Model -----------------------------------------------------
 
 # Make the needed matrices
-salary21SubsetForward <- salary21Subset%>%
+salaryAllSeasonsSubsetForward <- salaryAllSeasonsSubset%>%
   filter(position == 2)
-model_x <- salary21SubsetForward %>%
+model_x <- salaryAllSeasonsSubsetForward %>%
   select(-cap_hit) %>%
   as.matrix()
-model_y <- salary21SubsetForward$cap_hit
+model_y <- salaryAllSeasonsSubsetForward$cap_hit
 
 # Make the lasso fit plot
 library(glmnet)
@@ -131,12 +127,12 @@ saveRDS(importantLassoNames, file = "RawData/lasso_important_names_forward.rds")
 # Defense Lasso Model -----------------------------------------------------
 
 # Make the needed matrices
-salary21SubsetDefense <- salary21Subset%>%
+salaryAllSeasonsSubsetDefense <- salaryAllSeasonsSubset%>%
   filter(position == 1)
-model_x <- salary21SubsetDefense %>%
+model_x <- salaryAllSeasonsSubsetDefense %>%
   select(-cap_hit) %>%
   as.matrix()
-model_y <- salary21SubsetDefense$cap_hit
+model_y <- salaryAllSeasonsSubsetDefense$cap_hit
 
 # Make the lasso fit plot
 library(glmnet)
