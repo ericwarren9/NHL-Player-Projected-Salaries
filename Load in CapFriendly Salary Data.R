ca5d92322b924c088112f56f5f92b37e7816b482
Eric Warren <erwarren@ncsu.edu>
2022-07-27 17:13:57 -0400

    Documents needed to access data used for Salary Project

--- Load in CapFriendly Salary Data.R
+++ Load in CapFriendly Salary Data.R
@@ -31,8 +31,9 @@ library(lubridate)
 # 2010-11 Season
 capFriendlyData10 <- capFriendlyData %>%
   filter(season == "2010-11") %>%
-  mutate(ref_date = mdy(12312010),
-         age = year(ref_date)- year(birthdate),
+  mutate(ref_date = mdy(07012010),
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) < 7, year(ref_date) - year(birthdate), 0),
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) >= 7, year(ref_date) - year(birthdate) - 1, age),
          total_cap = 59400000,
          percent_cap_hit = round(cap_hit / total_cap, 6)) %>%
   mutate(type = ifelse(contract_number == 1, "Entry_Level", contract_number),
@@ -48,8 +49,9 @@ write_rds(capFriendlyData10, "RawData/CapFriendly 2010-11 Data.rds")
 # 2011-12 Season
 capFriendlyData11 <- capFriendlyData %>%
   filter(season == "2011-12") %>%
-  mutate(ref_date = mdy(12312011),
-         age = year(ref_date)- year(birthdate),
+  mutate(ref_date = mdy(07012011),
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) < 7, year(ref_date) - year(birthdate), 0),
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) >= 7, year(ref_date) - year(birthdate) - 1, age),
          total_cap = 64300000,
          percent_cap_hit = round(cap_hit / total_cap, 6)) %>%
   mutate(type = ifelse(contract_number == 1, "Entry_Level", contract_number),
@@ -65,8 +67,9 @@ write_rds(capFriendlyData11, "RawData/CapFriendly 2011-12 Data.rds")
 # 2012-13 Season
 capFriendlyData12 <- capFriendlyData %>%
   filter(season == "2012-13") %>%
-  mutate(ref_date = mdy(12312012),
-         age = year(ref_date)- year(birthdate),
+  mutate(ref_date = mdy(07012012) - 1,
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) < 7, year(ref_date) - year(birthdate), 0),
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) >= 7, year(ref_date) - year(birthdate) - 1, age),
          total_cap = 60000000,
          percent_cap_hit = round(cap_hit / total_cap, 6)) %>%
   mutate(type = ifelse(contract_number == 1, "Entry_Level", contract_number),
@@ -82,8 +85,9 @@ write_rds(capFriendlyData12, "RawData/CapFriendly 2012-13 Data.rds")
 # 2013-14 Season
 capFriendlyData13 <- capFriendlyData %>%
   filter(season == "2013-14") %>%
-  mutate(ref_date = mdy(12312013),
-         age = year(ref_date)- year(birthdate),
+  mutate(ref_date = mdy(07012013) - 1,
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) < 7, year(ref_date) - year(birthdate), 0),
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) >= 7, year(ref_date) - year(birthdate) - 1, age),
          total_cap = 64300000,
          percent_cap_hit = round(cap_hit / total_cap, 6)) %>%
   mutate(type = ifelse(contract_number == 1, "Entry_Level", contract_number),
@@ -99,8 +103,9 @@ write_rds(capFriendlyData13, "RawData/CapFriendly 2013-14 Data.rds")
 # 2014-15 Season
 capFriendlyData14 <- capFriendlyData %>%
   filter(season == "2014-15") %>%
-  mutate(ref_date = mdy(12312014),
-         age = year(ref_date)- year(birthdate),
+  mutate(ref_date = mdy(07012014) - 1,
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) < 7, year(ref_date) - year(birthdate), 0),
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) >= 7, year(ref_date) - year(birthdate) - 1, age),
          total_cap = 69000000,
          percent_cap_hit = round(cap_hit / total_cap, 6)) %>%
   mutate(type = ifelse(contract_number == 1, "Entry_Level", contract_number),
@@ -116,8 +121,9 @@ write_rds(capFriendlyData14, "RawData/CapFriendly 2014-15 Data.rds")
 # 2015-16 Season
 capFriendlyData15 <- capFriendlyData %>%
   filter(season == "2015-16") %>%
-  mutate(ref_date = mdy(12312015),
-         age = year(ref_date)- year(birthdate),
+  mutate(ref_date = mdy(07012015),
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) < 7, year(ref_date) - year(birthdate), 0),
+         age = ifelse(as.numeric(substr(birthdate, 6, 7)) >= 7, year(ref_date) - year(birthdate) - 1, age),
          total_cap = 71400000,
          percent_cap_hit = round(cap_hit / total_cap, 6)) %>%
   mutate(type = ifelse(contract_number == 1, "Entry_Level", contract_number),
